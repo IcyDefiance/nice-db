f69de1bf4cbb842c19140944082af45e69c64900
IcyDefiance <icydefiance@gmail.com>
2019-04-28 12:46:20 -0400

    Smaller config

--- webpack.config.ts
+++ webpack.config.ts
@@ -7,48 +7,17 @@ const config: Configuration = {
 	entry: "./src/index.tsx",
 	devtool: "inline-source-map",
 	target: "node",
-	node: {
-		__dirname: false,
-		__filename: false,
-	},
+	node: { __dirname: false, __filename: false },
 	module: {
 		rules: [
-			{
-				test: /\.tsx?$/,
-				use: "ts-loader",
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.scss$/,
-				use: [
-					// fallback to style-loader in development
-					MiniCssExtractPlugin.loader,
-					"css-loader",
-					"sass-loader",
-				],
-			},
-			{
-				test: /\.node$/,
-				use: "node-loader",
-			},
+			{ test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
+			{ test: /\.scss$/, use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"] },
+			{ test: /\.node$/, use: "node-loader" },
 		],
 	},
-	plugins: [
-		new MiniCssExtractPlugin({
-			// Options similar to the same options in webpackOptions.output
-			// both options are optional
-			filename: "[name].css",
-			chunkFilename: "[id].css",
-		}),
-	],
-	resolve: {
-		extensions: [".tsx", ".ts", ".js"],
-		plugins: [new TsconfigPathsPlugin()],
-	},
-	output: {
-		filename: "bundle.js",
-		path: resolve(__dirname, "dist"),
-	},
+	plugins: [new MiniCssExtractPlugin({ filename: "[name].css", chunkFilename: "[id].css" })],
+	resolve: { extensions: [".tsx", ".ts", ".js"], plugins: [new TsconfigPathsPlugin()] },
+	output: { filename: "bundle.js", path: resolve(__dirname, "dist") },
 	mode: process.env.NODE_ENV === "production" ? "production" : "development",
 };
 
