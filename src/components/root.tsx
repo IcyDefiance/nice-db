a3355266f493ce6af2aa943d6715c8d903aa46d5
IcyDefiance <icydefiance@gmail.com>
2019-04-27 23:27:08 -0400

    Fix horizontal scrollbar

--- src/components/root.tsx
+++ src/components/root.tsx
@@ -24,28 +24,30 @@ export function Root() {
 						<Icon icon={miDatabasePlus} /> Add Connection
 					</Button>
 				</Col>
-				<Col className="d-flex flex-wrap m-n2">
-					{conns.map((conn, i) => (
-						<Fade key={i} appear in>
-							<Card style={{ width: "18rem" }} className="m-2">
-								<Card.Body>
-									<div className="position-relative">
-										<Button
-											variant="link"
-											className="abs-top-right text-body p-1 m-n2"
-											onClick={() => handleDeleteClick(i)}
-										>
-											<Icon icon={miDelete} />
-										</Button>
-									</div>
-									<Card.Title>
-										{conn.host}:{conn.port}
-									</Card.Title>
-									<b>User:</b> {conn.user}
-								</Card.Body>
-							</Card>
-						</Fade>
-					))}
+				<Col>
+					<div className="d-flex flex-wrap m-n2">
+						{conns.map((conn, i) => (
+							<Fade key={i} appear in>
+								<Card style={{ width: "18rem" }} className="m-2">
+									<Card.Body>
+										<div className="position-relative">
+											<Button
+												variant="link"
+												className="abs-top-right text-body p-1 m-n2"
+												onClick={() => handleDeleteClick(i)}
+											>
+												<Icon icon={miDelete} />
+											</Button>
+										</div>
+										<Card.Title>
+											{conn.host}:{conn.port}
+										</Card.Title>
+										<b>User:</b> {conn.user}
+									</Card.Body>
+								</Card>
+							</Fade>
+						))}
+					</div>
 				</Col>
 			</Row>
 
